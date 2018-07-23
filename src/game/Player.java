3f223b8878e31d6f938f3a9d4703b066810ca556
penguin344 <cviola14580@gmail.com>
2018-07-23 03:14:23 -0400

    Add documentation to the Player class.

--- src/game/Player.java
+++ src/game/Player.java
@@ -8,6 +8,8 @@ import alignments.Alignment;
 import net.dv8tion.jda.core.entities.User;
 import roles.Role;
 
+/**A Player contains the information representing a person in a mafia game,
+ * and is used to process the results of actions on each player.*/
 public class Player {
 	
 	private User user;
@@ -18,51 +20,65 @@ public class Player {
 	private boolean isAlive = true;
 	private boolean hooked = false;
 	
+	/**Construct a Player for User u.*/
 	public Player(User u){
 		user = u;
 		identifier = u.getName()+"#"+u.getDiscriminator();
 	}
 	
+	/**Sets this Player's role to r.*/
 	public void setRole(Role r){
 		role = r;
 	}
 	
+	/**Returns this Player's role.*/
 	public Role getRole(){
 		return role;
 	}
 	
+	/**Returns the User object for the user this Player represents.*/
 	public User getUser(){
 		return user;
 	}
 	
+	/**Returns the message this Player needs from their role.*/
 	public String getNightMessage(Game g){
 		return role.roleMessageForThisNight(g);
 	}
 	
+	/**Sets the alignment of this Player to a.*/
 	public void setAlignment(Alignment a){
 		alignment = a;
 	}
 	
+	/**Returns the Player's username#discriminator combination.*/
 	public String getIdentifier(){
 		return identifier;
 	}
 	
+	/**Returns true iff this Player is currently alive.*/
 	public boolean isAlive(){
 		return isAlive;
 	}
 	
+	/**Returns true iff this Player is currently hooked.*/
 	public boolean isHooked(){
 		return hooked;
 	}
 	
+	/**Returns the alignment of this Player.*/
 	public Alignment getAlignment(){
 		return alignment;
 	}
 	
+	/**Returns true iff this Player is aligned with the cops.*/
 	public boolean isCop(){
 		return alignment.equals(Alignment.getAlignment("cops"));
 	}
 	
+	/**Sends the input string as a private message to this Player,
+	 * and returns a future which is completed when the message is successfully sent.
+	 */
 	public CompletableFuture<Boolean> privateMessage(String s){
 		if(s.equals("")){
 			return CompletableFuture.completedFuture(true);
@@ -76,26 +92,32 @@ public class Player {
 		return future;
 	}
 	
+	/**Append the input results to this Player's list of results.*/
 	public void appendResult(String s){
 		results.add(s);
 	}
 	
+	/**Clear the Player's list of results.*/
 	public void resetResults(){
 		results.clear();
 	}
 	
+	/**Get a string of all the current results for this Player.*/
 	public String getResultsMessage(){
 		return String.join("\n", results);
 	}
 	
+	/**Kill this Player.*/
 	public void kill(){
 		isAlive = false;
 	}
 	
+	/**Hook this Player.*/
 	public void hook(){
 		hooked = true;
 	}
 	
+	/**Prepare this Player for the next day, resetting all necessary fields.*/
 	public void nightReset(){
 		hooked = false;
 		role.resetTarget();
