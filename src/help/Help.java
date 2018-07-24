b940f7808c29b03787eacae7a8ebe48c5d3db4fa
penguin344 <cviola14580@gmail.com>
2018-07-24 01:35:32 -0400

    Add missing space in two help strings.

--- src/help/Help.java
+++ src/help/Help.java
@@ -21,14 +21,14 @@ public class Help {
 			case "sanecop":
 				return "Targets a user at night and is told whether the target is aligned with them. Does not know they are sane.";
 			case "insanecop":
-				return "Targets a user at night and is told the target is aligned with them if the target is not aligned with them,"
+				return "Targets a user at night and is told the target is aligned with them if the target is not aligned with them, "
 						+ "and vice-versa. Does not know they are insane.";
 			case "paranoidcop":
 				return "Targets a user at night and is told that the target is not aligned with them. Does not know they are paranoid.";
 			case "naivecop":
 				return "Targets a user at night and is told that the target is aligned with them. Does not know they are naive.";
 			case "wolf":
-				return "On Night 0, targets a user at night to prevent their action from working. After Night 0, targets a user at"
+				return "On Night 0, targets a user at night to prevent their action from working. After Night 0, targets a user at "
 						+ "night to kill them.";
 			default:
 				return "The provided role is not recoginzed as a role.";
