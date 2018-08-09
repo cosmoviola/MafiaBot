58be0310eac84fcd65151107335b8f4a7d66e4af
penguin344 <cviola14580@gmail.com>
2018-08-08 22:57:28 -0400

    Implement redirector role.

---
+++ src/roles/Redirector.java
@@ -0,0 +1,28 @@
+package roles;
+
+import actions.Redirect;
+
+public class Redirector extends Role {
+	
+	public Redirector(int priority){
+		Redirect red = new Redirect(priority, g -> true);
+		for(String key : red.getKeywords()){
+			actions.put(key, red);
+		}
+	}
+
+	@Override
+	public String roleMessage() {
+		return "You are a redirector. Every night, you may target two users to redirect one user's actions to the other user.";
+	}
+
+	@Override
+	public String cardFlip() {
+		return "Redirector";
+	}
+
+	@Override
+	public String getTrueName() {
+		return "Redirector";
+	}
+}
