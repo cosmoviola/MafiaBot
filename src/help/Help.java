c45e4c885ecdd8c2ead9662688bda192d43b1cbf
penguin344 <cviola14580@gmail.com>
2018-08-12 03:09:29 -0400

    Add help strings for new roles.

--- src/help/Help.java
+++ src/help/Help.java
@@ -31,6 +31,23 @@ public class Help {
 			case "wolf":
 				return "On Night 0, targets a user at night to prevent their action from working. After Night 0, targets a user at "
 						+ "night to kill them.";
+			case "hooker":
+				return "Targets a user at night to prevent them from using their night actions.";
+			case "martyr":
+				return "Targets a user at night to force them to target the martyr instead of their target.";
+			case "bodyguard":
+			case "bg":
+				return "Targets a user at night to protect them from all kills.";
+			case "safeguard":
+			case "sg":
+				return "Targets a user at night to protect them from all non-killing actions.";
+			case "mayor":
+				return "Has no night action, but their vote is worth double a normal vote in the lynch.";
+			case "vanilla":
+			case "vanillager":
+				return "Has no special abilities.";
+			case "redirector":
+				return "Targets two users at night to redirect all of one user's actions to the second user.";
 			default:
 				return "The provided role is not recoginzed as a role.";
 		}
